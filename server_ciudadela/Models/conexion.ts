01a4225f7481c91643a86bd634d6ca107f2e8adb
juanes0uu <juanestebanstt@gmail.com>
2025-12-10 12:20:13 -0500

    cambio meh

--- server_ciudadela/Models/conexion.ts
+++ server_ciudadela/Models/conexion.ts
@@ -1,11 +1,11 @@
 import { Client } from "../Dependencies/deps.ts";
 
 export const conexion = await new Client().connect({
-  hostname: "localhost",
-  username: "root",
-  db: "geolocalizacion",
-  password: "",
-  port: 3306,
+  hostname: Deno.env.get("DB_HOST")!,
+  username: Deno.env.get("DB_USER")!,
+  password: Deno.env.get("DB_PASSWORD")!,
+  db: Deno.env.get("DB_NAME")!,
+  port: Number(Deno.env.get("DB_PORT")!),
 });
 
 // Devuelve un array de objetos [{columna: valor, ...}]
